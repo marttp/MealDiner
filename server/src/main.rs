7434715d7d5e6d6ac112a1780d3556c4dafe2be9
Thanaphoom Babparn <tp.babparn@gmail.com>
2024-12-07 12:31:46 +0900

    Add foundation codebase

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,11 +1,19 @@
-use axum::Router;
+mod handler;
+
 use axum::routing::get;
+use axum::Router;
+use dotenv::dotenv;
+use crate::handler::health_check_handler;
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new()
-        .route("/health", get(|| async { "OK" }));
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:8000")
-        .await.unwrap();
+    // Load env - If deploy in container solution. Env should available on injection.
+    dotenv().ok();
+    let defined_port = std::env::var("SERVER_PORT").unwrap_or("8000".to_string());
+    let app = Router::new().route("/health", get(health_check_handler));
+    let server_address = format!("{}:{}", "0.0.0.0", defined_port);
+    let listener = tokio::net::TcpListener::bind(server_address.clone())
+        .await
+        .unwrap();
     axum::serve(listener, app).await.unwrap();
 }
