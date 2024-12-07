7fa174846f271b40285c2856a38d3061c9da2480
Thanaphoom Babparn <tp.babparn@gmail.com>
2024-12-07 13:35:46 +0900

    Shorter import

--- server/src/order/model.rs
+++ server/src/order/model.rs
@@ -1,5 +1,6 @@
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
+use chrono::{DateTime, Utc};
 use crate::menu::model::Menu;
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
@@ -8,7 +9,7 @@ pub struct Order {
     pub table_id: u32,
     pub menu: Menu,
     pub cooking_time_minutes: u32,
-    pub created_at: chrono::DateTime<chrono::Utc>,
+    pub created_at: DateTime<Utc>,
 }
 
 #[derive(Debug, Deserialize)]
