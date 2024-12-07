003ca22940d3379b7bd6854e37d427ad43691050
Thanaphoom Babparn <tp.babparn@gmail.com>
2024-12-07 14:00:58 +0900

    Delete menus inside handler

--- server/src/menu/handler.rs
+++ server/src/menu/handler.rs
@@ -1,17 +1,8 @@
-use crate::menu::model::Menu;
+use crate::internal_store::MENUS;
 use axum::response::IntoResponse;
 use axum::Json;
 use std::ops::Deref;
 use std::sync::Arc;
-use once_cell::sync::Lazy;
-
-static MENUS: Lazy<Arc<[Menu]>> = Lazy::new(|| {
-    Arc::new([
-        Menu::new("Ramen"),
-        Menu::new("Beef rice"),
-        Menu::new("Beer"),
-    ])
-});
 
 pub async fn get_available_menus() -> impl IntoResponse {
     let menus = Arc::clone(&MENUS);
