251bf4007bf5e5b34ff6b3e9af7e8271697e7fd4
Thanaphoom Babparn <tp.babparn@gmail.com>
2024-12-07 13:48:33 +0900

    Add internal config function

--- server/src/config/handler.rs
+++ server/src/config/handler.rs
@@ -2,9 +2,14 @@ use axum::Json;
 use axum::response::IntoResponse;
 use crate::config::model::Config;
 
-pub async fn get_configs() -> impl IntoResponse {
+pub fn get_config_internally() -> Config {
     let max_tables = std::env::var("AVAILABLE_TABLES").unwrap_or("10000".to_string());
     let config = Config::new((1, max_tables.parse::<u32>().unwrap()));
+    config
+}
+
+pub async fn get_configs() -> impl IntoResponse {
+    let config = get_config_internally();
     let json_response = serde_json::json!({
         "status": "success",
         "data": config
